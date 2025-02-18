47c9af8cfcd83d803972c5bd43ac99f0cd327308
Стас Пивоваров <stas.povovarov@headmade.pro>
2025-02-18 19:48:21 +0300

    Updates

---
+++ _next/static/chunks/app/layout-bad12af3c82b67e3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2195:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2032,23)),Promise.resolve().then(n.t.bind(n,4007,23))},4007:()=>{},2032:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[77,441,517,358],()=>s(2195)),_N_E=e.O()}]);
